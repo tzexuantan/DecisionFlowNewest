fdcbc5676d3b17247bcfdb89de179549ae6e1ef4
natlong <2401643@sit.singaporetech.edu.sg>
2024-10-03 22:10:02 +0800

    Add other menu options

--- streamlit/chatbot/chatbot_response.py
+++ streamlit/chatbot/chatbot_response.py
@@ -1,6 +1,12 @@
 import streamlit as st
 
-options_list = [
+menu_list = [
+    "Recruitment-related Questions",
+    "Visualizations?",
+    "Just wanna have a chat?"
+]
+
+recruitment_qns_list = [
     "Recommend degrees or skills for a job position",
     "Jobs I can apply for with my qualifications",
     "Find a job quickly after graduation",
@@ -8,35 +14,47 @@ options_list = [
     "Industries in high demand"
 ]
 
+visualizations_list = [
+
+]
+
+
 # Function to display options
-def display_options():
+def display_options(options_list):
     st.write("Please select an option:")
     for idx, option in enumerate(options_list):
         # Use a unique key for each button by combining a string with the index
         if st.button(option, key=f"option_{idx}"):
             st.session_state.selected_option = option
-            handle_option(option)  # Handle the option immediately upon clicking
-            st.rerun()  # Force rerun after the state is updated
+            handle_option(st.session_state.selected_option)
+
+def handle_option(option):
+    st.session_state.messages.append({"User": "user", "message": option})
+    if option == menu_list[0]:
+        display_options(recruitment_qns_list)
+    elif option in recruitment_qns_list:
+        handle_recruitment_option(st.session_state.selected_option)
+
 
 # Function to handle the selected option
-def handle_option(option):
+def handle_recruitment_option(option):
     # Append the selected option to chat history
     st.session_state.messages.append({"user": "user", "message": option})
 
     # Generate bot's response based on the selected option
-    if option == options_list[0]:
+    if option == recruitment_qns_list[0]:
         bot_response = "To excel in a job position, degrees in relevant fields, alongside skills like critical thinking and communication, are essential."
         
-    elif option == options_list[1]:
+    elif option == recruitment_qns_list[1]:
         bot_response = "Based on your qualifications, you can apply for jobs such as Data Analyst, Software Developer, or Marketing Specialist."
         
-    elif option == options_list[2]:
+    elif option == recruitment_qns_list[2]:
         bot_response = "To find a job quickly, consider internships, networking events, and job fairs as they can provide valuable opportunities."
         
-    elif option == options_list[3]:
+    elif option == recruitment_qns_list[3]:
         bot_response = "Yes, the reputation and resources of your school can influence your employment opportunities."
         
-    elif option == options_list[4]:
+    elif option == recruitment_qns_list[4]:
         bot_response = "Currently, industries such as Technology, Healthcare, and Renewable Energy are in high demand."
         
     else:
--- streamlit/chatbot/commands/chatbot.py
+++ streamlit/chatbot/commands/chatbot.py
@@ -2,10 +2,6 @@ import streamlit as st
 from chatbot.chatbot_response import *
 
 def chatbot():
-    # User Profile in the header
-    col1, col2, col3 = st.columns([1, 6, 1])
-    with col3:
-        st.image("https://lh3.googleusercontent.com/a/ACg8ocK0_IEiBum5fdzAh439e_1u_1-aRonEW_MwD5ay17MbeZ9rlw=s64", width=64)
 
     # Initialize session state for chat history and selected option
     if "messages" not in st.session_state:
@@ -18,6 +14,6 @@ def chatbot():
     display_messages()
 
     # Display chatbot options (buttons will always remain visible)
-    display_options()
+    display_options(menu_list)
 
     # No need to reset selected_option to None here anymore
